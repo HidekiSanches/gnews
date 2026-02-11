f289489eb060fb6a9cced4b8876a8614edb6a512
HidekiSanches <thiago.sanches@sptech.school>
2026-02-11 16:11:14 -0300

    feat: adiciona sql injection

--- src/main/java/com/gnews/fake/repository/ArticleRepository.java
+++ src/main/java/com/gnews/fake/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.gnews.fake.repository;
 import com.gnews.fake.domain.Article;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -17,5 +20,12 @@ public class ArticleRepository {
 
     public List<Article> findAll() {
         return Collections.unmodifiableList(articles);
+    }
+
+    public List<Article> findByTitle(String title) throws SQLException {
+        String sql = "SELECT * FROM articles WHERE title = '" + title + "'";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        return mapResult(rs);
     }
 }
